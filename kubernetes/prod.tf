50eebb263fbcb37466fd8f12e7aebcc7f5310452
Ondrej Sika <ondrej@ondrejsika.com>
2022-01-07 10:38:14 +0100

    feat(kubernetes): Add LoadBalancer for prod Kubernetes cluster

--- kubernetes/prod.tf
+++ kubernetes/prod.tf
@@ -9,7 +9,53 @@ resource "digitalocean_kubernetes_cluster" "prod" {
   }
 }
 
+resource "digitalocean_loadbalancer" "prod" {
+  name   = digitalocean_kubernetes_cluster.prod.name
+  region = "fra1"
+
+  droplet_tag = "k8s:${digitalocean_kubernetes_cluster.prod.id}"
+
+  healthcheck {
+    port     = 80
+    protocol = "tcp"
+  }
+
+  forwarding_rule {
+    entry_port      = 80
+    target_port     = 80
+    entry_protocol  = "tcp"
+    target_protocol = "tcp"
+  }
+
+  forwarding_rule {
+    entry_port      = 443
+    target_port     = 443
+    entry_protocol  = "tcp"
+    target_protocol = "tcp"
+  }
+}
+
+resource "cloudflare_record" "k8s" {
+  zone_id = local.zone_id
+  name    = "k8s"
+  value   = digitalocean_loadbalancer.prod.ip
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "k8s-wildcard" {
+  zone_id = local.zone_id
+  name    = "*.k8s"
+  value   = cloudflare_record.k8s.hostname
+  type    = "CNAME"
+  proxied = false
+}
+
 output "kubeconfig-prod" {
   value     = digitalocean_kubernetes_cluster.prod.kube_config.0.raw_config
   sensitive = true
+}
+
+output "lb-ip-prod" {
+  value = digitalocean_loadbalancer.prod.ip
 }
