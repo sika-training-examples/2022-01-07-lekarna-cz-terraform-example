22b5fd40730adf2f8acad7ab269a113308546ab0
Ondrej Sika <ondrej@ondrejsika.com>
2022-01-07 11:00:44 +0100

    refactor(kubernetes): Use k8s module for prod & dev clusters

--- kubernetes/dev.tf
+++ kubernetes/dev.tf
@@ -1,61 +1,7 @@
-resource "digitalocean_kubernetes_cluster" "dev" {
+module "k8s--dev" {
+  source  = "../modules/k8s"
   name    = "demo-dev"
-  region  = "fra1"
+  record  = "k8s-dev"
+  zone_id = local.zone_id
   version = local.kubernetes_dev_version
-  node_pool {
-    name       = "demo-dev"
-    size       = "s-2vcpu-2gb"
-    node_count = 1
-  }
-}
-
-resource "digitalocean_loadbalancer" "dev" {
-  name   = digitalocean_kubernetes_cluster.dev.name
-  region = "fra1"
-
-  droplet_tag = "k8s:${digitalocean_kubernetes_cluster.dev.id}"
-
-  healthcheck {
-    port     = 80
-    protocol = "tcp"
-  }
-
-  forwarding_rule {
-    entry_port      = 80
-    target_port     = 80
-    entry_protocol  = "tcp"
-    target_protocol = "tcp"
-  }
-
-  forwarding_rule {
-    entry_port      = 443
-    target_port     = 443
-    entry_protocol  = "tcp"
-    target_protocol = "tcp"
-  }
-}
-
-resource "cloudflare_record" "k8s-dev" {
-  zone_id = local.zone_id
-  name    = "k8s-dev"
-  value   = digitalocean_loadbalancer.dev.ip
-  type    = "A"
-  proxied = false
-}
-
-resource "cloudflare_record" "k8s-dev-wildcard" {
-  zone_id = local.zone_id
-  name    = "*.k8s-dev"
-  value   = cloudflare_record.k8s-dev.hostname
-  type    = "CNAME"
-  proxied = false
-}
-
-output "kubeconfig-dev" {
-  value     = digitalocean_kubernetes_cluster.dev.kube_config.0.raw_config
-  sensitive = true
-}
-
-output "lb-ip-dev" {
-  value = digitalocean_loadbalancer.dev.ip
 }
--- kubernetes/prod.tf
+++ kubernetes/prod.tf
@@ -1,61 +1,7 @@
-resource "digitalocean_kubernetes_cluster" "prod" {
+module "k8s--prod" {
+  source  = "../modules/k8s"
   name    = "demo-prod"
-  region  = "fra1"
+  record  = "k8s"
+  zone_id = local.zone_id
   version = local.kubernetes_prod_version
-  node_pool {
-    name       = "demo-prod"
-    size       = "s-2vcpu-2gb"
-    node_count = 1
-  }
-}
-
-resource "digitalocean_loadbalancer" "prod" {
-  name   = digitalocean_kubernetes_cluster.prod.name
-  region = "fra1"
-
-  droplet_tag = "k8s:${digitalocean_kubernetes_cluster.prod.id}"
-
-  healthcheck {
-    port     = 80
-    protocol = "tcp"
-  }
-
-  forwarding_rule {
-    entry_port      = 80
-    target_port     = 80
-    entry_protocol  = "tcp"
-    target_protocol = "tcp"
-  }
-
-  forwarding_rule {
-    entry_port      = 443
-    target_port     = 443
-    entry_protocol  = "tcp"
-    target_protocol = "tcp"
-  }
-}
-
-resource "cloudflare_record" "k8s" {
-  zone_id = local.zone_id
-  name    = "k8s"
-  value   = digitalocean_loadbalancer.prod.ip
-  type    = "A"
-  proxied = false
-}
-
-resource "cloudflare_record" "k8s-wildcard" {
-  zone_id = local.zone_id
-  name    = "*.k8s"
-  value   = cloudflare_record.k8s.hostname
-  type    = "CNAME"
-  proxied = false
-}
-
-output "kubeconfig-prod" {
-  value     = digitalocean_kubernetes_cluster.prod.kube_config.0.raw_config
-  sensitive = true
-}
-
-output "lb-ip-prod" {
-  value = digitalocean_loadbalancer.prod.ip
 }
